5691da6ff601212944cc52bb9a61c3aed5271e8c
harite <harite@qq.com>
2015-01-01 01:14:44 +0800

    提交测试内容

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+'''
+@date: 15/1/1
+@author: harite
+@memo: V4Vedentta
+@description: desc 
+'''
+
+import sys
+import os
+
+__author__ = 'harite'
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+if __name__ == "__main__":
+    print "hello"
+
